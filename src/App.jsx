53423c4fc8a72f79a3cdc5ca08ba8453d87893a9
Adarsh Jha <jha01adarsh@gmail.com>
2026-06-25 22:35:38 +0530

    Project setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import AccordionParentContainer from './components/1-Accordion/AccordionParentContainer.jsx';
+
+function App() {
+  return (
+    <>
+      <AccordionParentContainer />
+    </>
+  );
+}
+
+export default App;
