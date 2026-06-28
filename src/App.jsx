394c23023c2510c1cb8ef31313b92d5b17602c39
Adarsh Jha <jha01adarsh@gmail.com>
2026-06-28 23:40:17 +0530

    star rating starter code

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,13 @@
-// import AccordionParentContainer from './components/1-Accordion/AccordionParentContainer.jsx';
-// import Faq from './components/2-FAQs/Faq.jsx';
-// import data from '../utils/data.json';
-import Tabs from './components/3-Tabs/Tabs.jsx';
-
-const tabsData = [
-  {
-    label: 'Profile',
-    content: <div>Profile Info Content</div>,
-  },
-  {
-    label: 'Dashboard',
-    content: <div>Dashboard Content</div>,
-  },
-  {
-    label: 'Settings',
-    content: <div>Settings Content</div>,
-  },
-  {
-    label: 'Invoice',
-    content: <div>Invoice Content</div>,
-  },
-];
+import Rating from './components/4-StarRating/Rating';
 
 function App() {
-
-const onTabChange = (index)=>{
-  console.log("Tab changed and index is : ",index);
-}
+  const onTabChange = (index) => {
+    console.log('Tab changed and index is : ', index);
+  };
 
   return (
     <>
-      <Tabs tabsData={tabsData} onTabChange={onTabChange}/>
+      <Rating />
     </>
   );
 }
---
+++ src/components/4-StarRating/Rating.jsx
@@ -0,0 +1,5 @@
+const Rating = () => {
+  return <div>Star Rating</div>;
+};
+
+export default Rating;
